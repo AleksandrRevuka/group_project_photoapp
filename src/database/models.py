ecef6f1ddad42c9aa110ce176e6f79cc89e20905
Gerenzeo <124388411+Gerenzeo@users.noreply.github.com>
2023-10-13 12:59:07 +0300

    Update

--- src/database/models.py
+++ src/database/models.py
@@ -45,6 +45,9 @@ class User(Base, BaseWithTimestamps):
     avatar: Mapped[str] = mapped_column(String(255), nullable=True)
     roles: Mapped[Enum] = mapped_column("roles", Enum(Role), default=Role.user)
 
+    pictures: Mapped[list["Picture"]] = relationship("Picture", back_populates="user", cascade="all, delete-orphan")
+    comments: Mapped[int] = relationship("Comment", back_populates="user", cascade="all, delete-orphan")
+
 picture_tags = Table(
     'picture_tags', 
     Base.metadata,
@@ -56,13 +59,14 @@ class Tag(Base, BaseWithTimestamps):
     __tablename__ = "tags"
     id: Mapped[int] = mapped_column(primary_key=True)
     tagname: Mapped[str] = mapped_column(String(50), nullable=False, unique=True)
+    pictures: Mapped[List["Picture"]] = relationship("Picture", secondary=picture_tags, back_populates="tags", cascade="all, delete-orphan")
 
 class Comment(Base, BaseWithTimestamps):
     __tablename__ = "comments"
     id: Mapped[int] = mapped_column(Integer, primary_key=True)
     text: Mapped[str] = mapped_column(String(200), nullable=False)
-    user_id: Mapped[int] = mapped_column(ForeignKey("users.id"))
-    picture_id: Mapped[int] = mapped_column(ForeignKey("pictures.id"))
+    picture: Mapped["Picture"] = relationship("Picture", back_populates="comments")
+    user: Mapped[int] = relationship("User", back_populates="comments")
 
 class Picture(Base, BaseWithTimestamps):
     __tablename__ = "pictures"
@@ -70,5 +74,6 @@ class Picture(Base, BaseWithTimestamps):
     name: Mapped[str] = mapped_column(String(100), nullable=False)
     description: Mapped[str] = mapped_column(String(250), nullable=False)
     picture_url: Mapped[str] = mapped_column(String(200), nullable=False)
-    create_by: Mapped[int] = mapped_column(ForeignKey("users.id"))
-    tags: Mapped[List["Tag"]] = relationship("Tag", secondary=picture_tags, backref="pictures")
+    user: Mapped["User"] = relationship("User", back_populates="pictures")
+    comments: Mapped[list["Comment"]] = relationship("Comment", back_populates="picture", cascade="all, delete-orphan" )
+    tags: Mapped[List["Tag"]] = relationship("Tag", secondary=picture_tags, back_populates="pictures")
